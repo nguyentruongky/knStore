499fc08285f1277f3a2331215593aa7adc047348
Ky Nguyen <kynguyen@Kys-MacBook-Pro-3.local>
2018-12-11 03:10:07 +0700

    Add must_have

---
+++ knStore/src/must_have/1.Setting.swift
@@ -0,0 +1,15 @@
+//
+//  1.Setting.swift
+//  knCollection
+//
+//  Created by Ky Nguyen Coinhako on 7/3/18.
+//  Copyright © 2018 Ky Nguyen. All rights reserved.
+//
+
+import Foundation
+
+typealias knSetting = Setting
+struct Setting {
+    static var baseUrl: String = ""
+    static var token: String?
+}
