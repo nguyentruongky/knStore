d5a7a0e423b5c88e5a3af497036046419418860d
Ky Nguyen <kynguyen@Kys-MacBook-Pro-3.local>
2018-12-10 16:57:21 +0700

    Init project

---
+++ knStore/project_files/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  knStore
+//
+//  Created by Ky Nguyen Coinhako on 12/10/18.
+//  Copyright © 2018 Ky Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
