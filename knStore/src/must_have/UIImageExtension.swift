0cc414dfba96c811cbfe751e1c51e6f3a5ace540
Ky Nguyen <kynguyen@Kys-MacBook-Pro.local>
2020-03-13 09:50:16 +0700

    test

--- knStore/src/must_have/UIImageExtension.swift
+++ knStore/src/must_have/UIImageExtension.swift
@@ -10,6 +10,7 @@ import UIKit
 import Kingfisher
 import func AVFoundation.AVMakeRect
 
+// testing
 extension UIImageView {
     func downloadImage(from url: String?, placeholder: UIImage? = nil) {
         guard let url = url, let nsurl = URL(string: url) else { return }
